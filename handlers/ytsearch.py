2ff5851b718a6feaabf47bc06cff88a0d120773f
EfsaneStar <82456249+Mehmetbaba55@users.noreply.github.com>
2021-05-14 22:39:04 +0300

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -16,14 +16,14 @@ import pyrogram
 
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-@app.on_message(pyrogram.filters.command(["search"]))
-async def ytsearch(_, message: Message):
+@app.on_message(pyrogram.filters.command(["arama"]))
+async def ytarama(_, message: Message):
     try:
         if len(message.command) < 2:
             await message.reply_text("/search needs an argument!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("Searching....")
+        m = await message.reply_text("Arıyorum....")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
