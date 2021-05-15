45b8d35ea75c6bf84547d5622ce6a56670fb4003
EfsaneStar <82456249+Mehmetbaba55@users.noreply.github.com>
2021-05-15 05:32:47 +0300

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -1,18 +1,10 @@
-# Daisyxmusic (Telegram bot project )
-# Copyright (C) 2021  Inukaasith 
+# EfsaneStar (Telegram bot projesi)
+# Telif Hakkı (C) 2021 EfsaneStar 
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Bu program özgür bir yazılımdır: yeniden dağıtabilir ve/veya değiştirebilirsiniz. 
+# GNU Affero Genel Kamu Lisansı koşulları altında
+# Özgür Yazılım Vakfı tarafından yayınlandı, her iki sürüm de
+# Lisans veya (sizin istediğiniz gibi) sonraki herhangi bir sürüm.
 
 
 
