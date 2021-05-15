eb587e00acdae1a492ce91c719e3c19e5ca0d04f
EfsaneStar <82456249+Mehmetbaba55@users.noreply.github.com>
2021-05-15 05:29:12 +0300

    Update userbotjoin.py

--- handlers/userbotjoin.py
+++ handlers/userbotjoin.py
@@ -1,21 +1,5 @@
 
-# Daisyxmusic (Telegram bot project )
-# Copyright (C) 2021  Inukaasith 
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
+# EfsaneStar Katkılarıyla Yeni Muzik Yazılımını Denemeye Devam edeceğiz. 
 
 
 from callsmusic.callsmusic import client as USER
@@ -24,7 +8,7 @@ from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import UserAlreadyParticipant
 from helpers.decorators import errors, authorized_users_only
 
-@Client.on_message(filters.group & filters.command(["userbotkatil"]))
+@Client.on_message(filters.group & filters.command(["katil"]))
 @authorized_users_only
 @errors
 async def addchannel(client, message):
@@ -33,41 +17,41 @@ async def addchannel(client, message):
         invitelink = await client.export_chat_invite_link(chid)
     except:
         await message.reply_text(
-            "<b>Add me as admin of yor group first</b>",
+            "<b>Önce beni yor grubunun yöneticisi olarak ekle</b>",
         )
         return
 
     try:
         user = await USER.get_me()
     except:
-        user.first_name =  "TurkisVoicebot"
+        user.first_name =  "TurkisVoice"
 
     try:
         await USER.join_chat(invitelink)
         await USER.send_message(message.chat.id,"I joined here as you requested")
     except UserAlreadyParticipant:
         await message.reply_text(
-            "<b>helper already in your chat</b>",
+            "<b>sohbetinizde zaten yardımcı</b>",
         )
         pass
     except Exception as e:
         print(e)
         await message.reply_text(
             f"<b>🛑 Taşan Bekleme Hatası 🛑 \n kullanıcı {user.first_name} userbot için yoğun katılım istekleri nedeniyle grubunuza katılamadı! Kullanıcının grupta yasaklı olmadığından emin olun."
-            "\n\nOr manually add @DaisyXhelper to your Group and try again</b>",
+            "\n\nVeya Grubunuza el ile ekleyin ve yeniden deneyin</b>",
         )
         return
     await message.reply_text(
-            "<b>helper userbot joined your chat</b>",
+            "<b>yardımcı userbot sohbetinize katıldı</b>",
         )
     
-@USER.on_message(filters.group & filters.command(["userbotayril"]))
+@USER.on_message(filters.group & filters.command(["ayril"]))
 async def rem(USER, message):
     try:
         await USER.leave_chat(message.chat.id)
     except:  
         await message.reply_text(
-            f"<b>User couldn't leave your group! May be floodwaits."
-            "\n\nOr manually kick me from to your Group</b>",
+            f"<b>Kullanıcı grubunuzdan ayrılamadı! Floodwaits olabilir."
+            "\n\nYa da beni manuel olarak grubunuzdan tekmelersiniz.</b>",
         )
         return
